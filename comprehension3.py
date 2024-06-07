721fd3d259e3a8ec191d975553d8cd38c7fd7b7c
AnahitMargaryan <anahitmargaryan638@gmail.com>
2024-06-08 01:57:19 +0400

    23

---
+++ comprehension3.py
@@ -0,0 +1,8 @@
+# Exercise 3. Vowels in a Word
+# Create a list of vowels present in a given word.
+
+word = "dictionary"
+vowels = ["a","e","i","o","u"]
+vowels_list = [letter for letter in word if letter in vowels]
+print(vowels_list)
+        
