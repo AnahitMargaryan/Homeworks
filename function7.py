b926860efb1365d66652ce244208cb93bb681506
AnahitMargaryan <anahitmargaryan638@gmail.com>
2024-06-08 01:11:07 +0400

    11.7

---
+++ function7.py
@@ -0,0 +1,7 @@
+#Exercise .Write a Python function to check whether a number is in a given range.
+
+def number_in_range(num,start,end):
+    return start< num <end
+print(number_in_range(4,0,10))
+
+
