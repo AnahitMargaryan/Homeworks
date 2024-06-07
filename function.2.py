bdf7700a559f1e095e2d4bf2a00b30a0550ba1e9
AnahitMargaryan <anahitmargaryan638@gmail.com>
2024-06-08 01:21:13 +0400

    12.2

---
+++ function.2.py
@@ -0,0 +1,7 @@
+#2.  Power Function
+# Write a function that takes a base and an exponent and returns the result of raising the
+# base to the exponent.
+
+def power(base, exponent):
+    return base ** exponent
+print(power(2,3))
