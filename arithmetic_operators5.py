49d78379914bb848826b9868cc1221cd6ecbdf24
AnahitMargaryan <anahitmargaryan638@gmail.com>
2024-06-08 00:06:37 +0400

    4.5

---
+++ arithmetic_operators5.py
@@ -0,0 +1,12 @@
+
+#Exercise 5. The program prompts the user their birth year. Assuming a person’s age
+#is a non-negative integer not exceeding 120, output the user’s age or the
+#words “Incorrect Year”. The sample outputs assume it’s currently the year
+#2016. If you are writing this program during any other year, the correct
+#answers may differ. Store the value of the current year in a variable
+
+current_year = 2016
+Hayks_birth = 1996
+Maris_birth = 2008
+print(current_year - Hayks_birth)
+print(current_year - Maris_birth)
