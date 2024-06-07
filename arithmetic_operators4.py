c3b3cff3a8540070fef45cab8902752e9f8143e4
AnahitMargaryan <anahitmargaryan638@gmail.com>
2024-06-08 00:05:33 +0400

    4.4

---
+++ arithmetic_operators4.py
@@ -0,0 +1,12 @@
+
+#Exercise 4. Input two positive integers, and output a line describing their relation.
+#Follow the sample format.
+
+a = 8
+b = 13
+if a > b:
+    print("8 > 13")
+elif a < b:
+    print("8 < 13")
+else:
+    print("8 = 13")
