db189d434744e897b36d8ba5b5b5242a4b84b1de
AnahitMargaryan <anahitmargaryan638@gmail.com>
2024-06-08 01:27:14 +0400

    12.7

---
+++ function.7.py
@@ -0,0 +1,28 @@
+#7. Find youngest student
+# Write a function that takes a dictionary with information about students. Return info
+# about the youngest student.
+
+# students_info = {
+# 'student1': {
+# 'name': 'Ann',
+# 'age': 18,
+# 'subjects': ['Math', 'Geography', 'English'],
+# 'scores': [7, 8, 8, 6],
+# },
+# 'student2': {
+# 'name': 'Mary',
+# 'age': 20,
+# 'subjects': ['English', 'Biology', 'History'],
+# 'scores': [7, 6, 9, 10],
+# },
+# 'student3': {
+# 'name': 'Suzan',
+# 'age': 21,
+# 'subjects': ['Computer Science', 'Statistics', 'Psychology'],
+# 'scores': [8, 10, 7, 9],
+# },
+# }
+
+# def youngest_student(students_info):
+#     for key,value in students_info.items():
+#         if students_info["student1"]["age"] > ["student2"]["age"]>["student3"]["age"]:
