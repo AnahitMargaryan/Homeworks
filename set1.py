47666d9782ff678d7525b83824aa5e4a00fdd51d
AnahitMargaryan <anahitmargaryan638@gmail.com>
2024-06-08 00:44:23 +0400

    Update set1.py

---
+++ set1.py
@@ -0,0 +1,6 @@
+# Exercise 1. Write a Python function that takes two sets as input and returns a new set containing
+#elements that are present in both input sets.
+
+food1 = { "banana", "tomato", "radish", "coconut", "mango", "cabbage", "strawberry" }
+food2 = { "grapes", "lemon", "banana", "strawberry", "carrot", "tomato", "orange" }
+print(food1 & food2)
