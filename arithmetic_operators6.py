3b295e80a3d6abe60b6dad21e3be8cf8e9f66719
AnahitMargaryan <anahitmargaryan638@gmail.com>
2024-06-08 00:07:34 +0400

    4.6

---
+++ arithmetic_operators6.py
@@ -0,0 +1,20 @@
+#Exercise 6. Input three integers. Output the word “Sorted” if the numbers are listed in
+#a non-increasing or non-decreasing order and “Unsorted” otherwise.
+
+#example 1.
+a = 8
+b = 7
+c = 15
+if a < b < c:
+    print ("Sorted")
+else: 
+    print("Unsorted")
+
+#example 2.
+a = 4
+b = 13
+c = 28
+if a < b < c:
+    print("Sorted")
+else:
+    print("Unsorted")
