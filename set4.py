ccdcf01f50204f45dbebbb9a708429a6c21a3d4e
AnahitMargaryan <anahitmargaryan638@gmail.com>
2024-06-08 00:46:29 +0400

    8.4

---
+++ set4.py
@@ -0,0 +1,6 @@
+# Exercise 4. Write a Python function that takes two sets as input and returns a new set containing
+#elements that are present in either of the input sets, but not in both.
+ 
+grades1 = {8,9,10,7,8,9,8,8,10}
+grades2 = {8,9,9,7,6,8,5,9,10}
+print(grades2 - grades1)
