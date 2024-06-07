e447e398d972a371fdbee9340e5acbe286caa62e
AnahitMargaryan <anahitmargaryan638@gmail.com>
2024-06-08 00:21:58 +0400

    5.6

---
+++ for_loop6.py
@@ -0,0 +1,7 @@
+#Exercise 6. Turn every item of a list into its square.
+
+numbers = [4,12,8,15]
+final_numbers = []
+for digits in numbers:
+    final_numbers.append(digits ** 2)
+print(final_numbers)
